a2b84bed5284b00215939aacce755fdfa5dbffd7
soundchaser128 <soundchaser128@gmail.com>
2024-10-25 16:03:17 +0200

    fix: actually use the custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    extend: {},
-  },
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
   daisyui: {
     themes: [
       {
         valentine: {
+          fontFamily: "Outfit Variable,sans-serif",
           // eslint-disable-next-line @typescript-eslint/no-var-requires
           ...require("daisyui/src/theming/themes")["valentine"],
           "--rounded-btn": "0.5rem",
