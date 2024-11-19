909294314a3ef7bf598693bc43a4da8288cb8f13
soundchaser128 <soundchaser128@gmail.com>
2024-11-19 08:52:44 +0100

    hmm

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,41 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
-import wasm from "vite-plugin-wasm";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [wasm(), sveltekit()],
+export default defineConfig(
+  async () =>
+    /** @type {import('vite').UserConfig} */
+    ({
+      plugins: [sveltekit()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
+      optimizeDeps: {
+        exclude: ["@evolu/react", "@sqlite.org/sqlite-wasm"],
+        include: ["react-dom"],
+      },
+      worker: { format: "es" },
 
-    worker: {
-      format: "es",
-      plugins: () => [wasm()],
-    },
-  },
-}));
+      // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+      //
+      // 1. prevent vite from obscuring rust errors
+      clearScreen: false,
+      // 2. tauri expects a fixed port, fail if that port is not available
+      server: {
+        port: 1420,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+          ? {
+              protocol: "ws",
+              host,
+              port: 1421,
+            }
+          : undefined,
+        watch: {
+          // 3. tell vite to ignore watching `src-tauri`
+          ignored: ["**/src-tauri/**"],
+        },
+      },
+    }),
+);
