f2494c61a2d9bfaf2eb7b760ecc86c474388cbb3
soundchaser128 <soundchaser128@gmail.com>
2024-12-23 12:22:48 +0100

    fix broken import

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -1,6 +1,5 @@
 import { loadMnemonic } from "$lib/service/mnemonic";
 import { ErpyStorage } from "$lib/storage";
-import { subscribeToSyncState } from "$lib/sync-state.svelte";
 import { invoke } from "@tauri-apps/api/core";
 
 export const load = async () => {
@@ -8,8 +7,6 @@ export const load = async () => {
   const storage = new ErpyStorage(mnemonic);
   const config = await storage.getConfig();
   const activeModel: string | null = await invoke("active_model", { config });
-
-  subscribeToSyncState(storage);
 
   return {
     activeModel: activeModel || undefined,
